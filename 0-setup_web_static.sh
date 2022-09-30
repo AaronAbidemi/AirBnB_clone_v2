80487c490e6a3eff2f270aca34021f504db08590
Aaron Abidemi <99512094+AaronAbidemi@users.noreply.github.com>
2022-09-30 08:54:20 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,18 @@
 #!/usr/bin/env bash
-# Prepare your web servers
-
-## Update server
-sudo apt-get -y update
-sudo apt-get -y upgrade
-
-## Install NGINX
-sudo apt-get -y install nginx
-
-## Creates directories
-sudo mkdir -p /data/web_static/shared /data/web_static/releases/test
-
-## Write Hello World in index with tee command
-echo "Hello World" | sudo tee /data/web_static/releases/test/index.html
-
-## Create Symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-## Change owner and group like ubuntu
-sudo chown -R ubuntu:ubuntu /data
-
-## Add new configuration to NGINX
-sudo sed -i "/listen 80 default_server;/ a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-
-## Restart NGINX
+# Setup a web servers for the deployment of web_static.
+apt update -y
+apt install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
